ef68c2da623f5a8a931e629e204d9fd8d597fe42
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-09-17 10:42:18 +0200

    feat(setup): setup

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import { Blockchain } from '../blockchain';
-import { Wallet } from '../wallet';
-import { TransactionPool } from '../transaction-pool';
-import { WalletsPool } from '../wallets-pool';
-
-// create a new blockchain instance
-const blockchain = new Blockchain();
-const transactionPool = new TransactionPool([]);
-const walletsPool = new WalletsPool([]);
 
 const serverHealthCheck = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({
@@ -15,66 +6,4 @@ const serverHealthCheck = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-const blocksStatus = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json(blockchain.chain);
-};
-
-const getWallet = (req: Request, res: Response, next: NextFunction) => {
-    const wallet = walletsPool?.wallets[0]; // to remove
-    return res.status(200).json({ publicKey: wallet.publicKey, privateKey: wallet.privateKey, balance: wallet.balance });
-};
-
-const getWalletByPrivateKey = (req: Request, res: Response, next: NextFunction) => {
-    const { privateKey } = req.params;
-
-    const wallet = walletsPool.getWalletByPrivateKey(privateKey);
-    return res.status(200).json(wallet);
-};
-
-const getWalletByPublicKey = (req: Request, res: Response, next: NextFunction) => {
-    const { publicKey } = req.params;
-
-    const wallet = walletsPool.getWalletByPublicKey(publicKey);
-    return res.status(200).json(wallet);
-};
-
-const getWalletsPool = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json(walletsPool?.wallets);
-};
-
-const createWallet = (req: Request, res: Response, next: NextFunction) => {
-    const wallet = new Wallet(Date.now().toString());
-    const serializedWallet = { publicKey: wallet?.publicKey, privateKey: wallet?.privateKey, balance: wallet?.balance, secret: wallet?.secret };
-    walletsPool.addWallet(serializedWallet);
-    console.log(`New wallet added`, serializedWallet);
-    return res.status(200).json(serializedWallet);
-};
-
-const transactionsStatus = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json(transactionPool?.transactions);
-};
-
-const createTransaction = (req: Request, res: Response, next: NextFunction) => {
-    const { to, amount, type, publicKey } = req.body;
-    // getWallet
-    const senderWallet = walletsPool.getWalletByPublicKey(publicKey);
-    const wallet = new Wallet(senderWallet?.secret);
-
-    const transaction = wallet.createTransaction(to, amount, type, blockchain, transactionPool);
-
-    console.log(transaction);
-
-    transactionPool.addTransaction(transaction);
-    const block = blockchain.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
-
-    return res.status(200).json({ block: block, transaction: transaction.output });
-};
-
-const mineBlock = (req: Request, res: Response, next: NextFunction) => {
-    const block = blockchain.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
-    return res.status(200).json({ data: block });
-};
-
-export default { createWallet, serverHealthCheck, getWallet, blocksStatus, transactionsStatus, createTransaction, mineBlock, getWalletsPool, getWalletByPrivateKey };
+export default { serverHealthCheck };
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,16 @@
 import express from 'express';
-import cors from 'cors';
-import http from 'http';
 import bodyParser from 'body-parser';
-import logging from './config/logging';
-import config from './config/config';
-import routes from './routes';
 
-const NAMESPACE = 'Server';
+//get the port from the user or set the default port
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 //create a new app
 const app = express();
 
-//use cors middleware
-app.use(
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT']
-    })
-);
-
-//logging the request
-app.use((req, res, next) => {
-    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP -[${req.socket.remoteAddress}]`);
-
-    res.on(`finish`, () => {
-        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.statusCode}]`);
-    });
-    next();
-});
-
-// parse the request
-app.use(bodyParser.urlencoded({ extended: false }));
+//using the body parser middleware
 app.use(bodyParser.json());
 
-// rules of API
-app.use((req, res, next) => {
-    // remove in production
-    res.header('Access-Control-Allow-Origin', '*');
-
-    if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
-        return res.status(200).json({});
-    }
-
-    next();
+// app server configurations
+app.listen(HTTP_PORT, () => {
+    console.log(`listening on port ${HTTP_PORT}`);
 });
-
-// Routes
-app.use('/api', routes);
-
-// Error handling
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-
-    res.status(404).json({
-        message: error.message
-    });
-});
-
-const httpServer = http.createServer(app);
-
-httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
