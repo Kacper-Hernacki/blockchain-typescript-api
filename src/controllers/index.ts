1efd7576f02dfadf0bba1d48d86e99f63082bf09
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-09-18 14:25:15 +0200

    feat(setup): routing and controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { Blockchain } from '../blockchain';
+
+const blockchain = new Blockchain();
 
 const serverHealthCheck = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({
@@ -6,4 +9,14 @@ const serverHealthCheck = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export default { serverHealthCheck };
+const blocksStatus = (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).json(blockchain.chain);
+};
+
+const mineBlock = (req: Request, res: Response, next: NextFunction) => {
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+    return res.status(200).json({ data: block });
+};
+
+export default { serverHealthCheck, blocksStatus, mineBlock };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,7 @@ import controller from '../controllers/index';
 const router = express.Router();
 
 router.get('/check', controller.serverHealthCheck);
+router.get('/blocks', controller.blocksStatus);
+router.post('/mine', controller.mineBlock);
 
 export = router;
